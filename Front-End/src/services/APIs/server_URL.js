906d9b8d286559db26216e2c2f781b119928a64e
mohamedHamy <lwanym144@gmail.com>
2026-09-19 16:53:02 +0300

    Update production API URL and gitignore

--- Front-End/src/services/APIs/server_URL.js
+++ Front-End/src/services/APIs/server_URL.js
@@ -1,1 +1,4 @@
-export const BASE_URL = "http://localhost:1337";
+// export const BASE_URL = "http://localhost:1337";
+
+export const BASE_URL =
+  "https://triply-travel-website-production.up.railway.app";
